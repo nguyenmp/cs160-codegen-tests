974e5c11fc2400d1c2d3cef1712caffb81f4c895
Mark Nguyen <mpnguyen@umail.ucsb.edu>
2014-03-15 15:24:35 -0700

    Added error messages and red colors

--- tester.sh
+++ tester.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 
+noColor="\033[0m"
+redColor="\033[0;31m"
+
 make
-echo ""
 
 if [ $? -ne 0 ]; then
   exit 1;
@@ -17,6 +19,12 @@ fi
 for file in "${filepath[@]}"
 do
         echo "Testing $file"
-        ./lang < "$file" 1> /dev/null 2> test.s && gcc -g -m32 -o start start.c test.s && ./start
+
+        ./lang < "$file" 1> /dev/null 2> test.s && gcc -g -m32 -o start start.c test.s
+
+        if [ $? -eq 0 ]; then ./start;
+        else echo -e $redColor$(head -n 1 test.s)$noColor
+        fi
+
         echo ""
 done
